3c98824c8a232f0153ca8bd6c234aa89facdb65e
ArbitraryRenaissance <moorechanceus@gmail.com>
2023-05-06 14:25:21 -0400

    Changed the port number for Heroku compatibility

--- server.js
+++ server.js
@@ -72,6 +72,8 @@ app.post('/autocomplete', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
